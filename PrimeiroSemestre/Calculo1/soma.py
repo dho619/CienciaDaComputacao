c33f09b5488ecbc7b76d5eb0df1c5fe05614faef
beatrizds <beatrizds@unifeg.edu.br>
2019-05-14 20:32:29 -0300

    Programa de Soma

---
+++ PrimeiroSemestre/Calculo1/soma.py
@@ -0,0 +1,11 @@
+#Programa Simples para fazer a leitura de dois numeros e exibir o resultado
+
+print("Digite o primeiro numero: ")
+n1 = int(input())
+
+print("Digite o segundo numero: ")
+n2 = int(input())
+
+result = n1 + n2
+
+print("Resultado: ", result)
