1af2dd3ad9bf3ce1f504897aa7eb95645f0d2675
Geovane Barbosa <30576013+dho619@users.noreply.github.com>
2019-06-30 11:55:27 -0300

    Update Main.java

--- 5_Semestre/Compiladores/MinhaSplit/Main.java
+++ 5_Semestre/Compiladores/MinhaSplit/Main.java
@@ -1,14 +1,16 @@
 /*
-Na Classe MySplit existe dois metodos Split em sobrecarga:
+Na Classe MySplit existem dois metodos Split em sobrecarga:
 
 Split(String frase): Recebe uma frase por paramentro e retorna uma lista de char com todas as letras sem os espacos.Exemplo:
   
   Minha Frase de Teste => M,i,n,h,a,F,r,a,s,e,d,e,T,e,s,t,e 
 
-Split(String frase, char delimitador): Recebe uma frase e um delimitador por parametro e retorna uma lista de String das palavras da frase, de acordo com o delimitador, que deve ser um char. Exemplo:
+Split(String frase, char delimitador): Recebe uma frase e um delimitador por parametro e retorna uma lista de String das palavras da frase,
+de acordo com o delimitador, que deve ser um char. Exemplo:
   Minha*Frase*de*Teste => Minha,Frase,de,Teste
 
-obs: Foi usado um ArrayList, para conseguir um array dinamico, ja que nao e possivel saber o tanto de palavras tera no segundo caso ou quantos espacos tera (para subtrair do tamanho da string e criar um array) na frase para o primeiro caso.
+obs: Foi usado um ArrayList, para conseguir um array dinamico, ja que nao e possivel saber o tanto de palavras tera no segundo caso ou
+quantos espacos tera (para subtrair do tamanho da string e criar um array) na frase para o primeiro caso.
 
 */
 
