83da75875ffb3c06ae55e30a9109f4a08f80fee3
Geovane Barbosa <geovanebarbosacc@hotmail.com>
2020-06-28 14:22:59 -0300

    json mongo

---
+++ 7_Semestre/NoSQL/json.py
@@ -0,0 +1,46 @@
+{
+	"id": 1
+	"Description": "product_category",
+	"Products": [
+		  {
+		  	  "id": 1,
+			  "name": "Calvin Klein Women's Sheer Marquisette Demi Unlined Bra",
+			  "mrc": 36.00,
+			  "prince": 32.40,
+			  "pdp_url": "https://www.amazon.com/-/dp/B01NAVD98J?th=1&psc=1",
+			  "brand_name": "Calvin-Klein",
+			  "product_category": "Bras",
+			  "retailer": "Amazon US",
+			  "description": " An unlined demi cup bra featuring sheer, sexy fabrication and bold, contrasting edging with repeating Calvin Klein logo.",
+			  "rating": 4.5,
+			  "reviews": 47,
+			  "style_attributes": "[ 72% Nylon, 28% Elastane , Imported , hook and eye closure , Hand Wash , New super soft light weight sheer fabrication , Unlined demi cup , Sheer cup , Ultra light ABD breathable spacer cup technology ]",
+			  "sizes": [
+			        {
+						"id": XX,
+						"description": "30B"
+				     },
+				    {
+						"id": XX,
+						"description": "30C"
+				    },
+				    {
+						"id": XX,
+						"description": "30D"
+				    },
+				    {
+						"id": XX,
+						"description": "30DD"
+				    },
+				     …
+			],
+
+		  	"color": "Bare",
+		},
+		{
+			"id": '',
+			...
+		},
+		...
+	]
+}
